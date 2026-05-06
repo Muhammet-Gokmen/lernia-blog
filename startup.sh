684094ad9591cb4079d4423fe1da9e51f4108755
Muhammet-Gokmen <muhammetgokmen@gmail.com>
2026-05-06 12:09:49 +0200

    Fix: activate antenv from correct path /home/site/wwwroot/antenv

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/opt/python/3.11.14/bin/pip install -r /home/site/wwwroot/requirements.txt
+source /home/site/wwwroot/antenv/bin/activate
 
 cd /home/site/wwwroot/src
 
-/opt/python/3.11.14/bin/python manage.py collectstatic --noinput || echo "WARNING: collectstatic failed"
+python manage.py collectstatic --noinput || echo "WARNING: collectstatic failed"
 
-exec /opt/python/3.11.14/bin/gunicorn --bind=0.0.0.0:8000 --timeout=600 --workers=2 --chdir /home/site/wwwroot/src cblog.wsgi:application
+exec gunicorn --bind=0.0.0.0:8000 --timeout=600 --workers=2 --chdir /home/site/wwwroot/src cblog.wsgi:application
