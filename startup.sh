1fc47ebc3007a29a959352b6eb8359049a2f295c
Muhammet-Gokmen <muhammetgokmen@gmail.com>
2026-05-06 11:40:14 +0200

    Fix: add --chdir to gunicorn for correct module path

--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ cd /home/site/wwwroot/src
 
 python manage.py collectstatic --noinput || echo "WARNING: collectstatic failed"
 
-exec gunicorn --bind=0.0.0.0:8000 --timeout=600 --workers=2 cblog.wsgi:application
+exec gunicorn --bind=0.0.0.0:8000 --timeout=600 --workers=2 --chdir /home/site/wwwroot/src cblog.wsgi:application
